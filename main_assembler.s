e559f67daace44d9c257859081151a3e38dee421
Rvaudable <62510892+Rvaudable@users.noreply.github.com>
2020-08-08 18:22:49 +0200

    Small change

--- main_assembler.s
+++ main_assembler.s
@@ -5,6 +5,7 @@
 ;------------------------------------------------------------------------------
 ; Description
 ; This is a demonstration program to show basic code structures in assembly
+// comment
 ; HELLO YOU
 ; Includes
 #define  __ASSEMBLY__           1  ; Need this so the correct values in AT91SAM3U4.h are selected
